0cbfd59694fe4883bd5ce81da2fa2836af364ad2
hakanyvs <hakanyavas__2001@hotmail.com>
2023-12-12 15:26:36 +0300

    hud düzeni

--- VisualProgrammingProject/OrderPage.Designer.cs
+++ VisualProgrammingProject/OrderPage.Designer.cs
@@ -32,15 +32,15 @@
             this.BtnCold = new System.Windows.Forms.Button();
             this.BtnMilky = new System.Windows.Forms.Button();
             this.LstVwMenu = new System.Windows.Forms.ListView();
-            this.LstVwOrder = new System.Windows.Forms.ListView();
-            this.NumericPiece = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtTotal = new System.Windows.Forms.TextBox();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LstVwOrder = new System.Windows.Forms.ListView();
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NumericPiece = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
             this.BtnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.NumericPiece)).BeginInit();
             this.SuspendLayout();
@@ -48,9 +48,10 @@
             // btnHot
             // 
             this.btnHot.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnHot.Location = new System.Drawing.Point(71, 57);
+            this.btnHot.Location = new System.Drawing.Point(53, 46);
+            this.btnHot.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnHot.Name = "btnHot";
-            this.btnHot.Size = new System.Drawing.Size(210, 112);
+            this.btnHot.Size = new System.Drawing.Size(158, 91);
             this.btnHot.TabIndex = 0;
             this.btnHot.Text = "Hot";
             this.btnHot.UseVisualStyleBackColor = true;
@@ -59,9 +60,10 @@
             // BtnCold
             // 
             this.BtnCold.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.BtnCold.Location = new System.Drawing.Point(352, 57);
+            this.BtnCold.Location = new System.Drawing.Point(264, 46);
+            this.BtnCold.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnCold.Name = "BtnCold";
-            this.BtnCold.Size = new System.Drawing.Size(210, 112);
+            this.BtnCold.Size = new System.Drawing.Size(158, 91);
             this.BtnCold.TabIndex = 1;
             this.BtnCold.Text = "Cold";
             this.BtnCold.UseVisualStyleBackColor = true;
@@ -70,9 +72,10 @@
             // BtnMilky
             // 
             this.BtnMilky.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.BtnMilky.Location = new System.Drawing.Point(633, 57);
+            this.BtnMilky.Location = new System.Drawing.Point(475, 46);
+            this.BtnMilky.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnMilky.Name = "BtnMilky";
-            this.BtnMilky.Size = new System.Drawing.Size(210, 112);
+            this.BtnMilky.Size = new System.Drawing.Size(158, 91);
             this.BtnMilky.TabIndex = 2;
             this.BtnMilky.Text = "Milky";
             this.BtnMilky.UseVisualStyleBackColor = true;
@@ -85,12 +88,23 @@
             this.columnHeader2});
             this.LstVwMenu.FullRowSelect = true;
             this.LstVwMenu.HideSelection = false;
-            this.LstVwMenu.Location = new System.Drawing.Point(46, 310);
+            this.LstVwMenu.Location = new System.Drawing.Point(45, 196);
+            this.LstVwMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LstVwMenu.Name = "LstVwMenu";
-            this.LstVwMenu.Size = new System.Drawing.Size(207, 196);
+            this.LstVwMenu.Size = new System.Drawing.Size(190, 160);
             this.LstVwMenu.TabIndex = 3;
             this.LstVwMenu.UseCompatibleStateImageBehavior = false;
             this.LstVwMenu.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Coffees";
+            this.columnHeader1.Width = 130;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 50;
             // 
             // LstVwOrder
             // 
@@ -100,46 +114,13 @@
             this.columnHeader5});
             this.LstVwOrder.FullRowSelect = true;
             this.LstVwOrder.HideSelection = false;
-            this.LstVwOrder.Location = new System.Drawing.Point(554, 310);
+            this.LstVwOrder.Location = new System.Drawing.Point(377, 196);
+            this.LstVwOrder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LstVwOrder.Name = "LstVwOrder";
-            this.LstVwOrder.Size = new System.Drawing.Size(256, 196);
+            this.LstVwOrder.Size = new System.Drawing.Size(268, 160);
             this.LstVwOrder.TabIndex = 4;
             this.LstVwOrder.UseCompatibleStateImageBehavior = false;
             this.LstVwOrder.View = System.Windows.Forms.View.Details;
-            // 
-            // NumericPiece
-            // 
-            this.NumericPiece.Location = new System.Drawing.Point(361, 349);
-            this.NumericPiece.Name = "NumericPiece";
-            this.NumericPiece.Size = new System.Drawing.Size(120, 22);
-            this.NumericPiece.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(590, 524);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 25);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Total:";
-            // 
-            // txtTotal
-            // 
-            this.txtTotal.Location = new System.Drawing.Point(669, 527);
-            this.txtTotal.Name = "txtTotal";
-            this.txtTotal.Size = new System.Drawing.Size(100, 22);
-            this.txtTotal.TabIndex = 7;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Coffees";
-            this.columnHeader1.Width = 150;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Price";
-            this.columnHeader2.Width = 50;
             // 
             // columnHeader3
             // 
@@ -154,14 +135,41 @@
             // columnHeader5
             // 
             this.columnHeader5.Text = "Price";
-            this.columnHeader5.Width = 50;
+            // 
+            // NumericPiece
+            // 
+            this.NumericPiece.Location = new System.Drawing.Point(263, 252);
+            this.NumericPiece.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.NumericPiece.Name = "NumericPiece";
+            this.NumericPiece.Size = new System.Drawing.Size(90, 20);
+            this.NumericPiece.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(434, 394);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(522, 394);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(76, 20);
+            this.txtTotal.TabIndex = 7;
             // 
             // BtnAdd
             // 
             this.BtnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.BtnAdd.Location = new System.Drawing.Point(361, 416);
+            this.BtnAdd.Location = new System.Drawing.Point(263, 306);
+            this.BtnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnAdd.Name = "BtnAdd";
-            this.BtnAdd.Size = new System.Drawing.Size(120, 61);
+            this.BtnAdd.Size = new System.Drawing.Size(90, 50);
             this.BtnAdd.TabIndex = 8;
             this.BtnAdd.Text = "Add";
             this.BtnAdd.UseVisualStyleBackColor = true;
@@ -169,9 +177,9 @@
             // 
             // OrderPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(923, 590);
+            this.ClientSize = new System.Drawing.Size(692, 433);
             this.Controls.Add(this.BtnAdd);
             this.Controls.Add(this.txtTotal);
             this.Controls.Add(this.label1);
@@ -181,6 +189,7 @@
             this.Controls.Add(this.BtnMilky);
             this.Controls.Add(this.BtnCold);
             this.Controls.Add(this.btnHot);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "OrderPage";
             this.Text = "OrderPage";
             ((System.ComponentModel.ISupportInitialize)(this.NumericPiece)).EndInit();
