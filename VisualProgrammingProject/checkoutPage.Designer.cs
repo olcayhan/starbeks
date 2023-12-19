48f5afe48a5bf2765c38d022224d16d9c22d8b92
hakanyvs <hakanyavas__2001@hotmail.com>
2023-12-19 14:00:11 +0300

    First Start  CheckoutPage

--- VisualProgrammingProject/checkoutPage.Designer.cs
+++ VisualProgrammingProject/checkoutPage.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblSprNo = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lstviewFatura = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnOde = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -49,14 +49,14 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Sipariş No:";
             // 
-            // label2
+            // lblSprNo
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(124, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lblSprNo.AutoSize = true;
+            this.lblSprNo.Location = new System.Drawing.Point(124, 39);
+            this.lblSprNo.Name = "lblSprNo";
+            this.lblSprNo.Size = new System.Drawing.Size(44, 16);
+            this.lblSprNo.TabIndex = 1;
+            this.lblSprNo.Text = "label2";
             // 
             // label3
             // 
@@ -67,44 +67,19 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Fatura";
             // 
-            // listView1
+            // lstviewFatura
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lstviewFatura.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(218, 125);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(310, 237);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(629, 396);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Hesabı Öde";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(287, 396);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 16);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Toplam";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(356, 389);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 6;
+            this.lstviewFatura.HideSelection = false;
+            this.lstviewFatura.Location = new System.Drawing.Point(218, 125);
+            this.lstviewFatura.Name = "lstviewFatura";
+            this.lstviewFatura.Size = new System.Drawing.Size(310, 237);
+            this.lstviewFatura.TabIndex = 3;
+            this.lstviewFatura.UseCompatibleStateImageBehavior = false;
+            this.lstviewFatura.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
@@ -119,17 +94,43 @@
             // 
             this.columnHeader3.Text = "Fiyat";
             // 
+            // btnOde
+            // 
+            this.btnOde.Location = new System.Drawing.Point(629, 396);
+            this.btnOde.Name = "btnOde";
+            this.btnOde.Size = new System.Drawing.Size(113, 23);
+            this.btnOde.TabIndex = 4;
+            this.btnOde.Text = "Hesabı Öde";
+            this.btnOde.UseVisualStyleBackColor = true;
+            this.btnOde.Click += new System.EventHandler(this.btnOde_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(287, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Toplam:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(356, 389);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtPrice.TabIndex = 6;
+            // 
             // checkoutPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPrice);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnOde);
+            this.Controls.Add(this.lstviewFatura);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSprNo);
             this.Controls.Add(this.label1);
             this.Name = "checkoutPage";
             this.Text = "checkoutPage";
@@ -141,12 +142,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSprNo;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lstviewFatura;
+        private System.Windows.Forms.Button btnOde;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPrice;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
--- VisualProgrammingProject/checkoutPage.cs
+++ VisualProgrammingProject/checkoutPage.cs
@@ -12,9 +12,66 @@ namespace VisualProgrammingProject
 {
     public partial class checkoutPage : Form
     {
+
+        Dictionary<string, List<string>> kahveTurleri = new Dictionary<string, List<string>>
+        {
+            { "Sıcak Kahve", new List<string> { "Türk Kahvesi", "Espresso", "Americano" } },
+            { "Soğuk Kahve", new List<string> { "Frappe", "Cold Brew", "Iced Latte" } },
+            { "Sütlü Kahve", new List<string> { "Latte", "Cappuccino", "Macchiato" } }
+        };
+
+        Dictionary<string, double> kahveFiyatlari = new Dictionary<string, double>
+        {
+            { "Türk Kahvesi", 8.00 },
+            { "Espresso", 10.00 },
+            { "Americano", 12.00 },
+            { "Frappe", 15.00 },
+            { "Cold Brew", 14.00 },
+            { "Iced Latte", 16.00 },
+            { "Latte", 14.50 },
+            { "Cappuccino", 16.50 },
+            { "Macchiato", 15.50 }
+        };
+
+        Dictionary<string, int> FaturaPage = new Dictionary<string, int>();
+
         public checkoutPage()
         {
             InitializeComponent();
+        }
+        private void updateFatura()
+        {
+            lstviewFatura.Items.Clear();
+            double toplamfiyat = 0;
+
+            foreach (var Cfe in FaturaPage)
+            {
+                string cfename = Cfe.Key;
+                int cfepiece = Cfe.Value;
+                double cfeprice = kahveFiyatlari[cfename];
+                toplamfiyat += cfeprice * cfeprice;
+                ListViewItem item = new ListViewItem(cfename);
+                item.SubItems.Add(cfepiece.ToString());
+                item.SubItems.Add((cfeprice * cfepiece).ToString("C"));
+
+            }
+            txtPrice.Text = toplamfiyat.ToString();
+
+        }
+
+
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+            int fiyat = Convert.ToInt32(txtPrice.Text);
+            if(fiyat > 0) 
+            {
+                    
+            }
+            else
+            {
+                MessageBox.Show("Hesap Ödenmiş");
+            }
         }
     }
 }
