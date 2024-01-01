369c58b9eae2cb3587b6768f654ba0ae16354231
olcayhan <kolcayhan1@gmail.com>
2024-01-01 22:49:44 +0300

    class structure supported

--- VisualProgrammingProject/Order.cs
+++ VisualProgrammingProject/Order.cs
@@ -119,23 +119,4 @@ namespace VisualProgrammingProject
             return orderProducts;
         }
     }
-
-
-    public class OrderProduct
-    {
-        public int productID { get; set; }
-        public string productName { get; set; }
-        public double productPrice { get; set; }
-        public int productPiece { get; set; }
-        public int orderID { get; set; }
-
-        public OrderProduct(int productID, string productName, int productPiece, double productPrice,  int orderID)
-        {
-            this.productID = productID;
-            this.productName = productName;
-            this.productPrice = productPrice;
-            this.productPiece = productPiece;
-            this.orderID = orderID;
-        }
-    }
 }
---
+++ VisualProgrammingProject/OrderProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualProgrammingProject
+{
+    public class OrderProduct
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+        public int productPiece { get; set; }
+        public int orderID { get; set; }
+
+        public OrderProduct(int productID, string productName, int productPiece, double productPrice, int orderID)
+        {
+            this.productID = productID;
+            this.productName = productName;
+            this.productPrice = productPrice;
+            this.productPiece = productPiece;
+            this.orderID = orderID;
+        }
+    }
+
+}
